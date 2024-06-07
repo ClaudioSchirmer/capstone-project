9fff872c53c3cab83c22cb900c8bc5828967e2e2
andrew.song <comsi02@gmail.com>
2024-06-07 09:38:35 -0400

    - #22 add button for setting alarm

--- app/src/main/java/com/example/capstone_project/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/capstone_project/view/fragments/SettingsFragment.kt
@@ -1,13 +1,19 @@
 package com.example.capstone_project.view.fragments
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.capstone_project.R
+import com.example.capstone_project.databinding.FragmentSettingsBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,7 +22,18 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+
+        binding.btnPickTime.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                binding.tvTime.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this.requireContext(), timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+
+        return binding.root
     }
 }
