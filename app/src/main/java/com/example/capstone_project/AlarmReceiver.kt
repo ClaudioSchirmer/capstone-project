0242d1f224b584c8ca778378293f1eaafa305599
andrew.song <comsi02@gmail.com>
2024-06-08 12:53:24 -0400

    - #22 completed alarm notification

---
+++ app/src/main/java/com/example/capstone_project/AlarmReceiver.kt
@@ -0,0 +1,40 @@
+package com.example.capstone_project
+
+import android.Manifest
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0,
+            Intent(context, MainActivity::class.java),
+            PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context!!, context.getString(R.string.alarm_channel_id))
+            .setSmallIcon(R.drawable.outline_add_alert_24)
+            .setContentTitle(context.getString(R.string.alarm_title))
+            .setContentText(context.getString(R.string.alarm_text))
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        if (ActivityCompat.checkSelfPermission( context, Manifest.permission.POST_NOTIFICATIONS ) != PackageManager.PERMISSION_GRANTED ) {
+            return
+        }
+
+        notificationManager.notify(123, builder.build())
+    }
+}
