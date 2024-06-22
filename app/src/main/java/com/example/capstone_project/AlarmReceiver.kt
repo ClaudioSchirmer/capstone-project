95450645993a7b90db1e5464bffc6e7644a8d632
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-06-22 00:14:34 -0400

    - issue #22 fix set alarm and completed my sprint1

--- app/src/main/java/com/example/capstone_project/AlarmReceiver.kt
+++ app/src/main/java/com/example/capstone_project/AlarmReceiver.kt
@@ -14,13 +14,25 @@ class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        Log.d(this::class.simpleName, "Notification Alarm Start")
+        Log.d(this::class.simpleName, "Receive notification alarm")
 
         var calendar: Calendar = Calendar.getInstance()
         var notificationHelper: NotificationHelper = NotificationHelper(context)
         var nb: NotificationCompat.Builder = notificationHelper.getChannelNotification(calendar.time.toString())
         notificationHelper.getManager().notify(1, nb.build())
 
-        Log.d(this::class.simpleName, "Notification Alarm End")
+
+        var alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        var intent = Intent(context, AlarmReceiver::class.java)
+        var pendingIntent = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        alarmManager.cancel(pendingIntent)
+
+        calendar.add(Calendar.DATE, 1)
+        // for test
+        //calendar.add(Calendar.MINUTE, 2)
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        Log.d(this::class.simpleName, String.format("Set next alarm : %s", calendar.time.toString()))
     }
 }
