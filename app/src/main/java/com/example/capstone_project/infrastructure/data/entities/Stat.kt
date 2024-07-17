6ed69d38756329f497bdbb43748bd6a2ae784596
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-07-17 08:34:46 -0400

    - change database scheme

--- app/src/main/java/com/example/capstone_project/infrastructure/data/entities/Stat.kt
+++ app/src/main/java/com/example/capstone_project/infrastructure/data/entities/Stat.kt
@@ -8,8 +8,8 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "stats")
 data class Stat(
     @PrimaryKey(autoGenerate = true) val uid: Int? = null,
-    @ColumnInfo(name = "week") val week: Int,
-    @ColumnInfo(name = "wordUid") val wordUid: Int,
+    @ColumnInfo(name = "dateInfo") var dateInfo: String?,
+    @ColumnInfo(name = "wordUid") var wordUid: Int,
     @ColumnInfo(name = "isRemember") var isRemember: Boolean = false,
 ) {
     @Ignore var used: Boolean = false
