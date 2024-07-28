869cf92c296e506409b0856a251738861f89cd9e
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-07-28 16:47:27 -0400

    Picked fade-in animation for SpashScreen.

--- app/src/main/java/com/example/capstone_project/main/SplashActivity.kt
+++ app/src/main/java/com/example/capstone_project/main/SplashActivity.kt
@@ -6,6 +6,8 @@ import android.os.Handler
 import android.os.Looper
 import android.view.Window
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.capstone_project.R
 
@@ -16,10 +18,18 @@ class SplashActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
+        val logoImageView = findViewById<ImageView>(R.id.snapLearnCi)
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        logoImageView.startAnimation(fadeIn)
+
+        // rotate animation
+        // val rotate = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        //logoImageView.startAnimation(rotate)
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 3000)
     }
 }
