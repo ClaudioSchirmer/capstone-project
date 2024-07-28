259a2361b582c7152c608ca46f8a59678299798f
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-07-28 13:03:42 -0400

    issue #23 : Added the SnapLean CI image to use spash screen.

---
+++ app/src/main/java/com/example/capstone_project/main/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.capstone_project.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.capstone_project.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
